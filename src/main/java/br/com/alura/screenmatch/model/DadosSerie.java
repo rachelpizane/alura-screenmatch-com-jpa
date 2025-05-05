5832307c29e50d5b1eb6510579f2f77e562d0417
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-05-05 17:15:13 -0300

    refactor: Adicionadas novas propriedades ao registro DadosSerie

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -1,10 +1,18 @@
 package br.com.alura.screenmatch.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("Genre") String generos,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
-                         @JsonAlias("imdbRating") String avaliacao) {
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonAlias("Actores") String atores,
+                         @JsonAlias("Poster") String imagem,
+                         @JsonAlias("Plot") String sinopse
+                         
+                         ) {
 }
